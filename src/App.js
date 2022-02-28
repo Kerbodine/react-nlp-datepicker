489e4371ceb58a197bd72ba0c7fb1c6d678d3154
Michael Tong <michaelyuyangtong@gmail.com>
2022-02-28 14:51:40 +0800

    Upload code to GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import * as chrono from "chrono-node";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("");
+  const [dates, setDates] = useState([]);
+
+  const checkDate = (e) => {
+    const updatedText = e.target.value;
+    const results = chrono.parse(updatedText, Date.now(), {
+      forwardDate: true,
+      // ensures that date is after current day
+    });
+    if (results) {
+      console.log(results);
+      setDates(results.map((result) => result.start));
+    }
+    setText(updatedText);
+  };
+
+  const formatDate = (date) => {
+    return `${
+      date.knownValues.day ? date.knownValues.day : date.impliedValues.day
+    }/${
+      date.knownValues.month ? date.knownValues.month : date.impliedValues.month
+    }/${
+      date.knownValues.year ? date.knownValues.year : date.impliedValues.year
+    }/${date.knownValues.hour ? date.knownValues.hour : ""}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen grid place-items-center bg-gray-200">
+      <div className="space-y-2">
+        <input
+          value={text}
+          onChange={checkDate}
+          className="w-48 h-10 rounded-lg shadow-md bg-white px-3 outline-none"
+        />
+        {dates &&
+          dates.map((date, index) => (
+            <div
+              key={index}
+              className="px-1 py-0.5 text-sm bg-gray-400 text-white rounded-md"
+            >
+              {formatDate(date)}
+            </div>
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
